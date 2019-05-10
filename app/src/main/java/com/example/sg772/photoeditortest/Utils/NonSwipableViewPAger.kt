eae2a13a8533ac0ee9a257ac14fc981ac17f7783
Nuruddin <sg7720@gmail.com>
2019-05-10 15:20:41 +0300

    added custom view

---
+++ app/src/main/java/com/example/sg772/photoeditortest/Utils/NonSwipableViewPAger.kt
@@ -0,0 +1,44 @@
+package com.example.sg772.photoeditortest.Utils
+
+import android.content.Context
+import android.support.v4.view.ViewPager
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.animation.DecelerateInterpolator
+import android.widget.Scroller
+import java.lang.Exception
+import java.time.Duration
+
+class NonSwipableViewPAger : ViewPager {
+    constructor(context: Context) : super(context) {
+        setMyScroller()
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
+        setMyScroller()
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onTouchEvent(ev: MotionEvent?): Boolean {
+        return false
+    }
+    private fun setMyScroller() {
+        try {
+            val viewPager = ViewPager::class.java
+            val scroller = viewPager.getDeclaredField("mScroller")
+            scroller.isAccessible = true
+            scroller.set(this, MyScroller(context))
+        } catch (e:Exception){
+            e.printStackTrace()
+        }
+    }
+}
+
+class MyScroller(context: Context?):Scroller(context,DecelerateInterpolator()) {
+    override fun startScroll(startX: Int, startY: Int, dx: Int, dy: Int,duration: Int) {
+        super.startScroll(startX, startY, dx, dy,400)
+    }
+}
